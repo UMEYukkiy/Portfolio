f0bf26a1f0532c5164722faa1d4ee13ac82a7729
UMEYUKITO <umeyukkiy1027@gmail.com>
2025-10-29 01:29:19 +0900

    worksの追加

--- components/organisms/Works/page.tsx
+++ components/organisms/Works/page.tsx
@@ -1,3 +1,5 @@
+import Box from '@/components/atmos/Box/index'
+
 type FontProps = {
   titleFont?: string,
   bodyFont?: string
@@ -6,10 +8,23 @@ type FontProps = {
 export default function Works({ titleFont, bodyFont} : FontProps) {
   return (
     <main>
-      <div>
-        <h2 className={`text-3xl text-center ${titleFont}`}>Works</h2>
+      <div className="relative bg-white shadow-md text-black z-10 h-100 w-full pt-8 px-8">
+        <div>
+          <h2 className={`text-3xl text-center ${titleFont}`}>Works</h2>
+        </div>
+        <div className="flex gap-8 mt-8">
+          <Box
+            title="Demo1"
+            content="デモ表示1"
+            imageUrl="/images/study_image.jpg"
+          />
+          <Box
+            title="Demo2"
+            content="デモ表示2"
+            imageUrl="/images/study_image.jpg"
+          />
+        </div>
       </div>
     </main>
   )
-
 }
